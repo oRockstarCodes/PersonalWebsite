ed1e6ff84147ec259718ca929ae0ffad8680dd23
Rocky <rocky.wang2@student.tdsb.on.ca>
2025-04-21 00:16:35 -0400

    Added react router dom and Header

---
+++ RockyWangWebsite/src/views/HomeView.jsx
@@ -0,0 +1,18 @@
+import "./Homeview.css";
+import Header from "../components/Header.jsx";
+// import Hero from "../components/Hero.jsx";
+// import Feature from "../components/Feature.jsx";
+// import Footer from "../components/Footer.jsx";
+
+function HomeView() {
+    return (
+        <div>
+            <Header />
+            {/* <Hero />
+            <Feature />
+            <Footer /> */}
+        </div>
+    )
+}
+
+export default HomeView
